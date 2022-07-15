2a89937812219920458671438c159b90999054c7
James Robertson <digital.robertson@gmail.com>
2022-07-15 08:58:36 +0100

    feature: Now uses kj_reading for convenient title search and download to OGG.

--- kja.gemspec
+++ kja.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'kja'
-  s.version = '0.2.1'
-  s.summary = 'Downloads and plays King James Bible OGG files from rorbuilder.info'
+  s.version = '0.3.0'
+  s.summary = 'Downloads and plays King James Bible OGG files from jamesrobertson.me.uk'
   s.authors = ['James Robertson']
   s.files = Dir['lib/kja.rb']
-  s.add_runtime_dependency('dynarex', '~> 1.7', '>=1.7.22') 
+  s.add_runtime_dependency('dynarex', '~> 1.9', '>=1.9.11')
+  s.add_runtime_dependency('kj_reading', '~> 0.3', '>=0.3.0')
   s.signing_key = '../privatekeys/kja.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/kja'
 end
--- lib/kja.rb
+++ lib/kja.rb
@@ -4,6 +4,8 @@
 
 require 'dynarex'
 require 'tempfile'
+require 'rxfreader'
+require 'kj_reading'
 
 
 # description: Intended for use with the kj gem
@@ -25,41 +27,97 @@ require 'tempfile'
 
 class Kja
 
-  def initialize(audio_player: '/usr/bin/ogg123 -q', 
-                 ogg_baseurl: 'http://rorbuilder.info/r/ruby/kja/ogg')
+  def initialize(audio_player: '/usr/bin/ogg123 -q',
+                 ogg_baseurl: 'http://a0.jamesrobertson.me.uk/'  \
+                + 'rorb/r/ruby/kja/ogg', debug: false)
 
     @ogg_baseurl, @audio_player = ogg_baseurl, audio_player
+    @debug = debug
 
   end
-  
-  def duration(s)
-    
-    book_title, chapter_title, verse_no = fracture(s)    
-    
-    url = @ogg_baseurl + '/' + File.join(book_title, chapter_title, 'dir.xml')
 
-    dx = Dynarex.new url
-    ogg = dx.find_by_name chapter_title + '_' + verse_no + '.ogg'    
-    ogg.description[/\d+\.\d+$/].to_f
-
-  end  
-
-  def speak(s)
+  def download_ogg(s)
 
     ogg_filepath = fetch_ogg_filepath(s)
     url = @ogg_baseurl + '/' + ogg_filepath
-    audio, _ = RXFHelper.read(url)
-    
-    tmpfile = Tempfile.new('kva')
-    File.write tmpfile.path, audio
+    audio, _ = RXFReader.read(url)
 
-    command = @audio_player + ' ' + tmpfile.path
-    system command
+    tmpfile = Tempfile.new('kva')
+    File.write tmpfile.path + '.ogg', audio
+
+    return tmpfile.path + '.ogg'
 
   end
-  
+
+  def duration(s)
+
+    book_title, chapter_title, verse_no = fracture(s)
+
+    url = @ogg_baseurl + '/' + File.join(book_title, chapter_title, 'dir.xml')
+
+    dx = Dynarex.new url
+    ogg = dx.find_by_name chapter_title + '_' + verse_no + '.ogg'
+    ogg.description[/\d+\.\d+$/].to_f
+
+  end
+
+  def speak(title)
+
+    a = KjReading.verses title
+
+    a.each do |x|
+
+      filename = download_ogg(x.title)
+      command = @audio_player + ' ' + filename
+      system command
+
+    end
+
+  end
+
+
+  def download(s, outfile=nil)
+
+    puts 'downloading ...' if @debug
+
+    a = KjReading.verses s
+
+    downloads = a.map do |x|
+      download_ogg(x.title)
+    end
+
+    puts 'converting to wav: ' + downloads.inspect if @debug
+
+    wavfiles = downloads.map do |oggfile|
+
+      wavfile = oggfile.sub(/\.ogg$/,'.wav')
+      EasyAudioUtils.new(oggfile, wavfile).convert
+      sleep 1.4
+      File.basename(wavfile)
+
+    end
+
+    puts 'concatenating wav files ...' if @debug
+
+    # concat wav files
+    #
+    fullwavfile = Tempfile.new('kva').path + '.wav'
+    puts 'creating fullwavfile:' + fullwavfile.inspect if @debug
+    EasyAudioUtils.new(out: fullwavfile, working_dir: \
+                       File.dirname(fullwavfile)).concat(wavfiles, sample_rate: 22050)
+
+    # convert wav to ogg
+    #
+    fulloggfile = outfile || fullwavfile.sub(/\.wav$/, '.ogg')
+    puts 'creating fulloggfile: ' + fulloggfile.inspect if @debug
+    EasyAudioUtils.new(fullwavfile, fulloggfile).convert
+
+    'downloaded ' + fulloggfile
+
+  end
+
   private
-  
+
   def fracture(s)
 
     book_no, book_title, chapter_no, verse_no = \
@@ -68,16 +126,16 @@ class Kja
     book_title.gsub!(/\s/,'_')
     book_title = book_no + '_' + book_title if book_no
     chapter_title = book_title + '_' + chapter_no
-    
+
     return [book_title, chapter_title, verse_no]
   end
-  
+
   def fetch_ogg_filepath(s)
 
     book_title, chapter_title, verse_no = fracture(s)
     verse_title = chapter_title + '_' + verse_no
 
-    File.join(book_title, chapter_title, verse_title + '.ogg')    
+    File.join(book_title, chapter_title, verse_title + '.ogg')
   end
 
 end
